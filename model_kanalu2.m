331580a277d88162f9f52b21c2d1d471955f0d19
petrsvobodnik <svobodnikp@gmail.com>
2015-05-11 02:02:52 +0200

    Revert "pokus o parfor"
    This reverts commit 6138440208c0573269907f8e9bdc765ee34958f3.
    
    Conflicts:
    	hlavni1.asv
    	hlavni1.m
    	vypocet.m

--- model_kanalu2.m
+++ model_kanalu2.m
@@ -1,5 +1,6 @@
 function [ datasum ] = model_kanalu2( symboly1, h1, symboly2, h2, Pnoise )
-    nuly (numel(symboly1),1) = 0+.01j;  % je potøeba inicializovat s komplexním èíslem
+    nuly (numel(symboly1),1) = 0+.01j;
+%     nuly = zeros(numel(symboly1),1);
     sum = awgn (nuly, Pnoise);
     
     data1 = 10^(-abs(h1)/10) * symboly1;
